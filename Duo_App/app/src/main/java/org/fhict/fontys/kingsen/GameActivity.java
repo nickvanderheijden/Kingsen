d81d701cfa393137aaec9e6442808075d05fc4ff
NickvanderHeijden <nickvdh44@gmail.com>
2018-03-16 13:21:12 +0100

    working on  set of card in the array for the game

--- Duo_App/app/src/main/java/org/fhict/fontys/kingsen/GameActivity.java
+++ Duo_App/app/src/main/java/org/fhict/fontys/kingsen/GameActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.google.firebase.database.DataSnapshot;
 
 import org.fhict.fontys.kingsen.Objects.Card;
+import org.fhict.fontys.kingsen.Objects.DatabaseReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,14 +16,21 @@ public class GameActivity extends AppCompatActivity {
     ArrayList<Card> cards = new ArrayList<Card>();
 
 
-private void fetchData(DataSnapshot dataSnapshot){
-    cards.clear();
+    private void fetchData(DataSnapshot dataSnapshot) {
+        cards.clear();
 
-    for(DataSnapshot ds : dataSnapshot.getChildren()){
-        Card card = ds.getValue(Card.class);
-        cards.add(card);
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            Card card = ds.getValue(Card.class);
+            cards.add(card);
+        }
     }
-}
+
+    public ArrayList<Card> retrieve() {
+        DatabaseReference db;
+        return null;
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
